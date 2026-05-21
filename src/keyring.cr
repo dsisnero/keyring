37da0c57cca45ee894d86fbb421b2bd435376d29
Dominic Sisneros <dsisnero@gmail.com>
2026-05-21 11:16:12 -0600

    fix: load platform backends before generic fallbacks in main keyring.cr

--- src/keyring.cr
+++ src/keyring.cr
@@ -1,19 +1,15 @@
 require "./keyring/backend"
-require "./keyring/chainer_backend"
 require "./keyring/config"
 require "./keyring/credential"
 require "./keyring/encryption"
 require "./keyring/errors"
-require "./keyring/fail_backend"
-require "./keyring/file_backend"
 require "./keyring/logging"
 require "./keyring/metrics"
-require "./keyring/null_backend"
 require "./keyring/platform"
 require "./keyring/retryable"
 require "./keyring/circuit_breaker"
-require "./keyring/keyring"
 
+# Platform-specific backends register first (higher priority)
 {% if flag?(:linux) %}
   require "./keyring/kwallet_backend"
   require "./keyring/linux_backend"
@@ -26,3 +22,11 @@ require "./keyring/keyring"
 {% if flag?(:windows) %}
   require "./keyring/windows_backend"
 {% end %}
+
+# Generic backends register after platform backends (fallback)
+require "./keyring/file_backend"
+require "./keyring/chainer_backend"
+require "./keyring/null_backend"
+require "./keyring/fail_backend"
+
+require "./keyring/keyring"
