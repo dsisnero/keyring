5fbd52f022fec46eb703d4907c8d9b4dd73b3c3e
Dominic Sisneros <dsisnero@gmail.com>
2026-05-20 20:22:35 -0600

    fix: add LibWin32 compat aliases for forked win32cr
    - win32cr fork uses Win32cr::Security::Credentials instead of LibWin32
    - Add LibWin32 module aliases for CREDENTIALW, CREDENTIAL_ATTRIBUTEW,
      CRED_TYPE, CRED_PERSIST, FILETIME on Windows
    - Compat.cr already loaded before compilation via encryption.cr require

--- src/keyring/compat.cr
+++ src/keyring/compat.cr
@@ -8,4 +8,12 @@
     alias UInt32T = UInt32
     alias Int32T = Int32
   end
+
+  module LibWin32
+    alias CREDENTIALW = Win32cr::Security::Credentials::CREDENTIALW
+    alias CREDENTIAL_ATTRIBUTEW = Win32cr::Security::Credentials::CREDENTIAL_ATTRIBUTEW
+    alias CRED_TYPE = Win32cr::Security::Credentials::CRED_TYPE
+    alias CRED_PERSIST = Win32cr::Security::Credentials::CRED_PERSIST
+    alias FILETIME = Win32cr::Foundation::FILETIME
+  end
 {% end %}
